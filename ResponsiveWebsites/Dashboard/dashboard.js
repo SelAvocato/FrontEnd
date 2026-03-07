4316e62a2d8c09e5f0e56aa244b2bf4729ad3227
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-03-07 20:15:54 +0800

    Made it responsive for smaller devices

--- ResponsiveWebsites/Dashboard/dashboard.js
+++ ResponsiveWebsites/Dashboard/dashboard.js
@@ -1,9 +1,20 @@
 const dropdown = document.querySelector('.dashboard-dropdown');
 const dropdownSign = document.getElementById('dropdown-icon')
 const dropdownItems = document.getElementById('dropdown-items')
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const aside = document.querySelector('aside')
+const x = document.querySelector('.x')
 
 dropdown.addEventListener('click', () => {
 
     dropdownItems.classList.toggle('close');
     dropdownSign.classList.toggle('rotate')
+})
+
+hamburgerMenu.addEventListener('click', () => {
+    aside.classList.add('show')
+})
+
+x.addEventListener('click', () => {
+    aside.classList.remove('show')
 })
