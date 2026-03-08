2fdf3788b00052d2366f5e7c74d82ca0b060d13d
Braylle Jossiel Soberano <soberanobrayllecalingasan@gmail.com>
2026-03-08 12:00:58 +0800

    Finished the dashboard

--- ResponsiveWebsites/Dashboard/dashboard.js
+++ ResponsiveWebsites/Dashboard/dashboard.js
@@ -1,13 +1,17 @@
 const dropdown = document.querySelector('.dashboard-dropdown');
 const dropdownSign = document.getElementById('dropdown-icon')
-const dropdownItems = document.getElementById('dropdown-items')
+const dropdownList = document.getElementById('dropdown-list')
+const dropdownItems = document.querySelectorAll('#dropdown-list li')
 const hamburgerMenu = document.querySelector('.hamburger-menu')
 const aside = document.querySelector('aside')
 const x = document.querySelector('.x')
+const searchbox = document.querySelector('.searchbox')
+const searchIcon = document.getElementById('searchIcon')
+const searchInput = document.getElementById('search-input')
+const notifBell = document.querySelector('.notif-bell')
 
 dropdown.addEventListener('click', () => {
-
-    dropdownItems.classList.toggle('close');
+    dropdownList.classList.toggle('close');
     dropdownSign.classList.toggle('rotate')
 })
 
@@ -18,3 +22,21 @@ hamburgerMenu.addEventListener('click', () => {
 x.addEventListener('click', () => {
     aside.classList.remove('show')
 })
+
+searchIcon.addEventListener('click', () => {
+    searchInput.classList.toggle('showSearchboxInput')
+})
+
+searchInput.addEventListener('keydown', e => {
+    if(e.key === 'Enter') alert(`Sorry.. There are no results for: ${searchInput.value}`)
+})
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('click', e => {
+        document.querySelector('.active').classList.remove('active')
+        dropdownItem.classList.add('active')
+        aside.classList.remove('show')
+    })
+})
+
+notifBell.addEventListener('click', () => document.querySelector('.notification-container').classList.toggle('show'))
